f8d678d401d9303e03c05b09b94cb77c495fea03
Info Reqd <inforeqd51@gmail.com>
2017-04-07 19:14:49 +1000

    adds logic to smoothly increase header height, also overstretch

--- NonScrollingTableHeader/NonScrollingTableHeader/HeaderViewScrollDown.swift
+++ NonScrollingTableHeader/NonScrollingTableHeader/HeaderViewScrollDown.swift
@@ -36,31 +36,86 @@ class HeaderViewExpandHeightAtScrollDownDelegate: NSObject {
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         
         if let tableView = object as? UITableView {
-            let newContentOffset = change?[.newKey] as! CGPoint
             
-            if newContentOffset.y < 0 { //scrollview has scrolled south of (0,0) contentOffset, which will always be the case as after the collapse, the content offset of the scrollview is zero at the collapsed header height
-                
-                let oldContentOffset = change?[.oldKey] as! CGPoint
-                let offset = newContentOffset.y - oldContentOffset.y//highMinus - lowMinus = -ve
-                if offset < 0 { //Scrolling down
-                    let headerHeight = self.headerView.heightConstraint.constant
-                    let preferredMaximumHeight  = self.headerView.preferredMaximumHeight
-                    let preferredMinimumHeight = self.headerView.preferredMinimumHeight
-                    if headerHeight >= preferredMinimumHeight && //while the headerHeight is [preferredMin, preferredMax], adjust it
-                        headerHeight < preferredMaximumHeight
-                    {
-                        //there has been a scroll change and scrollView is not just tracking
-                        let finalHeight = finalHeaderHeight(contentOffset: newContentOffset,
-                                                            contentInset: tableView.contentInset,
-                                                            headerHeight: headerHeight,
-                                                            preferredMinimumHeight: self.headerView.preferredMinimumHeight,
-                                                            preferredMaximumHeight: preferredMaximumHeight)
-                        self.headerView.heightConstraint.constant = finalHeight
-                        
-                    }
-                }
-                
-            }
+            let newContentOffset = change?[.newKey] as! CGPoint
+            let oldContentOffset = change?[.oldKey] as! CGPoint
+            
+            //compute offset as vector which indicates the scroll direction
+            let offset = newContentOffset.y - oldContentOffset.y
+            
+
+            self.headerView.heightConstraint.constant = increasedHeight(of: self.headerView, by: offset, contentInset: tableView.contentInset, newContentOffset: newContentOffset, oldContentOffset: oldContentOffset)
+            
+            self.headerView.headerViewBottomEqualInnerHeaderBottom.constant = overstretchedValue(of: self.headerView, by: offset, newContentOffset: newContentOffset, oldContentOffset: oldContentOffset)
         }
     }
+    
+    /// decides if the scroll down is so that the real height of the headerView should increase upto preferred max
+    /// scrollview has scrolled south of (0,0) contentOffset, which will always be the case even after the collapse, the content offset of the scrollview is zero at the collapsed header height
+    ///
+    /// - newContentOffset: newContentOffset new value as returned by KVO
+    /// - Returns: true if height should decrease
+    func shouldIncreaseRealHeaderHeight(newContentOffset : CGPoint) -> Bool {
+        if newContentOffset.y < 0 { //if scrolling downwards from content offset (0,0)
+            return true //then actual height of the header view will increase
+        }else{
+            return false
+        }
+    }
+    
+    /// informs if the user is scrolling down
+    ///
+    /// - Parameters:
+    ///   - newContentOffset: newContentOffset new value as returned by KVO
+    ///   - oldContentOffset: oldContentOffset old value that was , by KVO
+    /// - Returns: true if scrolling down
+    func isScrollingDown(newContentOffset:CGPoint, oldContentOffset:CGPoint) -> Bool {
+        let offset = newContentOffset.y - oldContentOffset.y
+        if offset < 0 {
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    func increasedHeight(of headerView: HeaderView, by offset: CGFloat, contentInset: UIEdgeInsets, newContentOffset : CGPoint, oldContentOffset : CGPoint) -> CGFloat {
+        
+        if shouldIncreaseRealHeaderHeight(newContentOffset: newContentOffset) {
+            if isScrollingDown(newContentOffset: newContentOffset, oldContentOffset: oldContentOffset)
+            {
+                let finalHeight = finalHeaderHeight(deltaContentOffset: CGPoint(x: 0.0, y: offset),
+                                                    contentInset: contentInset,
+                                                    headerHeight: headerView.heightConstraint.constant,
+                                                    preferredMinimumHeight: headerView.preferredMinimumHeight,
+                                                    preferredMaximumHeight: headerView.preferredMaximumHeight)
+                return finalHeight
+            }
+        }
+        return headerView.heightConstraint.constant
+    }
+
+    func overstretchedValue(of headerView: HeaderView, by offset: CGFloat, newContentOffset : CGPoint, oldContentOffset : CGPoint) -> CGFloat {
+        if needsOverstretching(for: headerView, preferredMaxHeight: headerView.preferredMaximumHeight) {
+            if isScrollingDown(newContentOffset: newContentOffset, oldContentOffset: oldContentOffset) {
+                return increasedConstantForOverstretchinHeaderViewBottomEqualInnerHeaderBottomConstraint(headerView: headerView, deltaOffsetOfDownwardScroll: offset)
+            }
+        }
+        
+        return headerView.headerViewBottomEqualInnerHeaderBottom.constant
+    }
+    
+    func needsOverstretching(for headerView: HeaderView, preferredMaxHeight: CGFloat) -> Bool {
+        if headerView.heightConstraint.constant >= preferredMaxHeight {
+            return true
+        }else{
+            return false
+        }
+    }
+
+    func increasedConstantForOverstretchinHeaderViewBottomEqualInnerHeaderBottomConstraint(headerView: HeaderView, deltaOffsetOfDownwardScroll: CGFloat) -> CGFloat  {
+        let overstretchVector = headerView.headerViewBottomEqualInnerHeaderBottom.constant
+        let newOverstretchVector = overstretchVector + deltaOffsetOfDownwardScroll
+        return newOverstretchVector
+    }
+
 }
